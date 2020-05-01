0109808c2c6cdc7624e7dc494a37ee19e55ead75
Oaklee Ohmie <oakleeohmie@outlook.com>
2020-04-30 21:14:16 -0600

    added to app.js to render quiz component

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,16 @@
+import React, { Component } from 'react';
 
+class App extends Component {
+  renderQuiz() {
+    return (
+      <Quiz
+        answer={this.state.answer}
+        answerChoices={this.state.Choices}
+        questionId={this.state.questionId}
+        question={this.state.question}
+        questionTotal={colorQuestions.length}
+        onAnswerSelected={this.handleAnswerSelected}
+      />
+    );
+  }
+}
