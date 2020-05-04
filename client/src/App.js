11c74e775bb56a8d9c5e54fd3941d66b82fdbf98
Oaklee Ohmie <oakleeohmie@outlook.com>
2020-05-03 20:53:37 -0600

    finished quiz

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
+import update from 'react-addons-update';
+import colorQuestions from './api/colorQuestions';
 import Quiz from './components/Quiz';
+import Result from './components/Result';
+import './App.css';
 
 class App extends Component {
 
@@ -23,12 +27,76 @@ class App extends Component {
     this.handleAnswerSelected = this.handleAnswerSelected.bind(this);
   }
 
+  componentWillMount() {
+    const shuffledAnswerChoices = colorQuestions.map((question) => this.shuffleArray(question.answers));
+    this.setState({
+      // question: colorQuestions[0].question,
+      answerChoices: shuffledAnswerChoices[0]
+    });
+  }
 
+  shuffleArray(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  };
+
+  handleAnswerSelected(event) {
+    this.setUserAnswer(event.currentTarget.value);
+    if (this.state.questionId < colorQuestions.length) {
+      setTimeout(() => this.setNextQuestion(), 400);
+    } else {
+      setTimeout(() => this.setResults(this.getResults()), 400);
+    }
+  }
+  setUserAnswer(answer) {
+    const updatedAnswersCount = update(this.state.answersCount, {
+      [answer]: { $apply: (currentValue) => currentValue + 1 }
+    });
+    this.setState({
+      answersCount: updatedAnswersCount,
+      answer: answer
+    });
+  }
+  setNextQuestion() {
+    const counter = this.state.counter + 1;
+    const questionId = this.state.questionId + 1;
+    this.setState({
+      counter: counter,
+      questionId: questionId,
+      question: colorQuestions[counter].question,
+      answerChoices: colorQuestions[counter].answers,
+      answer: ''
+    });
+  }
+  getResults() {
+    const answersCount = this.state.answersCount;
+    const answersCountKeys = Object.keys(answersCount);
+    const answersCountValues = answersCountKeys.map((key) => answersCount[key]);
+    const maxAnswerCount = Math.max.apply(null, answersCountValues);
+
+    return answersCountKeys.filter((key) => answersCount[key] === maxAnswerCount);
+  }
+  setResults(result) {
+    if (result.length === 1) {
+      this.setState({ result: result[0] });
+    } else {
+      this.setState({ result: 'Undetermined' });
+    }
+  }
   renderQuiz() {
     return (
       <Quiz
         answer={this.state.answer}
-        answerChoices={this.state.Choices}
+        answerChoices={this.state.answerChoices}
         questionId={this.state.questionId}
         question={this.state.question}
         questionTotal={colorQuestions.length}
@@ -36,4 +104,17 @@ class App extends Component {
       />
     );
   }
+  renderResult() {
+    return (
+      <Result quizResult={this.state.result} />
+    );
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.result ? this.renderResult() : this.renderQuiz()}
+      </div>
+    );
+  }
 }
+export default App;
