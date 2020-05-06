939cb5c0e4c1797c37e9a5ab358e8e576292f144
Oaklee Ohmie <oakleeohmie@outlook.com>
2020-05-06 09:41:19 -0600

    added start quiz button to welcome page that will take user to quiz

--- client/src/welcome/Welcome.js
+++ client/src/welcome/Welcome.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import './Welcome.css';
 
 function Welcome() {
     return (
         <div className="welcome">
             <h2>Color Personality Quiz</h2>
             <p>This is a color personality quiz. Answer honestly.  </p>
+            <Button onClick={e => { e.preventDefault(); window.location.href = '/quiz' }}
+
+            >
+                START QUIZ
+            </Button>
         </div >
     );
 }
