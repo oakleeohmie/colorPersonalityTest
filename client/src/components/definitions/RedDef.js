c7064792054b446ea31c18bf8b029452884ddb34
Oaklee Ohmie <oakleeohmie@outlook.com>
2020-05-07 21:47:29 -0600

    altered the color definitions

--- client/src/components/definitions/RedDef.js
+++ client/src/components/definitions/RedDef.js
@@ -7,7 +7,7 @@ function RedDef() {
     return (
         <div className="redSummary">
             <h2 className="redTitle" >Roll out the red carpet!</h2>
-            <p className="redDescription">Reds are the power wielders of the world. Reds use logic, vision and determination. From a Red perspective, emotion has nothing to do with completing tasks. Reds are: Action-oriented, Assertive, Confident, Decisive, Determined, Disciplined, Obsessive, Independent, Logical, Pragmatic, Proactive, Productive, Responsible, and Task-Dominant.</p>
+            <p className="redDescription">Reds are the power wielders of the world. Reds use logic, vision and determination. From a Red perspective, emotion has nothing to do with completing tasks. Reds are action-oriented, assertive, confident, decisive, determined, independent, logical, proactive, and task-dominant. Reds can be controlling, insensitive, aggressive and impatient. Reds can also be very focused, pragmatic, disciplined, and responsible. If you're a red, you probably like to be right, be respected, be a leader and be productive.</p>
             <div className="takeAgainButton">
                 <Button onClick={e => { e.preventDefault(); window.location.href = '/quiz' }}
                     variant="contained"
