902bfb7be2b0e717fa3b936cde81cd1709941b00
Oaklee Ohmie <oakleeohmie@outlook.com>
2020-05-08 16:38:03 -0600

    changed package.json for heroku push

--- server.js
+++ server.js
@@ -12,11 +12,14 @@ var db = require("./models");
 var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
 // We need to use sessions to keep track of our user's login status
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
+
+
+app.use(express.static("public"));
+
 
 // Requiring our routes
 //require("./routes/html-routes.js")(app);
